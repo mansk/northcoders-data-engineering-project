a65604e9d5f753166b9248d7f1574631c5fe993d
mansk <mansk@users.noreply.github.com>
2024-05-14 14:04:26 +0100

    Ingestion Lambda: Use get_database_credentials helper in conn to db

--- src/connect.py
+++ src/connect.py
@@ -1,17 +1,22 @@
 import pg8000.native
 import os
 from dotenv import load_dotenv
+from src.get_db_creds import get_database_credentials
 
-load_dotenv(override=True)
+load_dotenv()
+os.environ['PG_USER'] = get_database_credentials()["username"]
+os.environ['PG_PASSWORD'] = get_database_credentials()["password"]
+os.environ['PG_DATABASE'] = get_database_credentials()["database"]
+os.environ['PG_HOST'] = get_database_credentials()["hostname"]
+os.environ['PG_PORT'] = str(get_database_credentials()["port"])
+
+
 
 def connect_db():
-  return pg8000.native.Connection(
-    user=os.getenv('PG_USER'),
-    password=os.getenv('PG_PASSWORD'),
-    database=os.getenv('PG_DATABASE'),
-    host=os.getenv('PG_HOST'),
-    port=os.getenv('PG_PORT')
-  )
-
-
-  
+    return pg8000.native.Connection(
+        user=os.getenv("PG_USER"),
+        password=os.getenv("PG_PASSWORD"),
+        database=os.getenv("PG_DATABASE"),
+        host=os.getenv("PG_HOST"),
+        port=os.getenv("PG_PORT"),
+    )
