00fc7ab1419bf47a18f0990357bcb27da7344be2
kedz-1 <khadar7@hotmail.co.uk>
2024-05-14 17:01:10 +0100

    terraform triggers and dummy lambda created

--- terraform/ingestionlambda.tf
+++ terraform/ingestionlambda.tf
@@ -1,8 +1,16 @@
 resource "aws_lambda_function" "ingestion_lambda" {
     function_name = "${var.team_prefix}ingestion-lambda"
+    filename = data.archive_file.lambda_ingestion_source.output_path
+    source_code_hash = data.archive_file.lambda_ingestion_source.output_base64sha256
     role = aws_iam_role.ingestion_lambda_role.arn
-    handler = n/a
-    runtime = n/a
+    handler = "dummyhandler.dummy_handler"
+    runtime = "python3.11"
 }
 
 
+
+data "archive_file" "lambda_ingestion_source" {
+    type = "zip"
+    output_path = "${path.module}/../zip/ingestion.zip"
+    source_dir = "${path.module}/../src/ingestion/"
+}
