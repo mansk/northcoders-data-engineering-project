4a3e90a6287ac6eddf2416ee54ccb4ba3579153e
mansk <mansk@users.noreply.github.com>
2024-05-14 16:16:28 +0100

    Connect to totesys db using credentials from Secrets Manager

--- src/connect.py
+++ src/connect.py
@@ -4,12 +4,11 @@ from dotenv import load_dotenv
 from src.get_db_creds import get_database_credentials
 
 load_dotenv()
-os.environ['PG_USER'] = get_database_credentials()["username"]
-os.environ['PG_PASSWORD'] = get_database_credentials()["password"]
-os.environ['PG_DATABASE'] = get_database_credentials()["database"]
-os.environ['PG_HOST'] = get_database_credentials()["hostname"]
-os.environ['PG_PORT'] = str(get_database_credentials()["port"])
-
+os.environ["PG_USER"] = get_database_credentials()["username"]
+os.environ["PG_PASSWORD"] = get_database_credentials()["password"]
+os.environ["PG_DATABASE"] = get_database_credentials()["database"]
+os.environ["PG_HOST"] = get_database_credentials()["hostname"]
+os.environ["PG_PORT"] = str(get_database_credentials()["port"])
 
 
 def connect_db():
