f0c02c70d3a19564f2cb8cd3088c9dfaa8523ed0
mansk <mansk@users.noreply.github.com>
2024-05-14 14:05:03 +0100

    Remove diagnostic print call from get_table helper

--- src/ingestion_lambda.py
+++ src/ingestion_lambda.py
@@ -1,4 +1,4 @@
-import boto3 
+import boto3
 import datetime
 import json
 import logging
@@ -6,24 +6,22 @@ from src.connect import connect_db
 from pg8000.native import identifier
 
 
-client= boto3.client('s3')
-logger= logging.getLogger()
+client = boto3.client("s3")
+logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 def get_table(table_name):
-  conn = None
-  try:
-    conn = connect_db()
-    query = f'SELECT * FROM {identifier(table_name)} LIMIT 10;'
-    result=conn.run(query)
-    output_list = [
-        dict(zip([ column['name'] for column in conn.columns], r))
-        for r in result
-    ]
-    print("result:", output_list )
-    return output_list
+    conn = None
+    try:
+        conn = connect_db()
+        query = f"SELECT * FROM {identifier(table_name)} LIMIT 10;"
+        result = conn.run(query)
+        output_list = [
+            dict(zip([column["name"] for column in conn.columns], r)) for r in result
+        ]
+        return output_list
 
-  finally:
-    if conn:
-      conn.close()
-
+    finally:
+        if conn:
+            conn.close()
