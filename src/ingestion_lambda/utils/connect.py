ecc3c59431cdd7f3a6d60e3e14e767a3a8f55c1d
mansk <mansk@users.noreply.github.com>
2024-05-15 16:34:02 +0100

    Ingestion lambda: Remove use of environment variables in connect.py

--- src/ingestion_lambda/utils/connect.py
+++ src/ingestion_lambda/utils/connect.py
@@ -1,21 +1,16 @@
 import pg8000.native
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from src.get_db_creds import get_database_credentials
 
-load_dotenv()
-os.environ["PG_USER"] = get_database_credentials()["username"]
-os.environ["PG_PASSWORD"] = get_database_credentials()["password"]
-os.environ["PG_DATABASE"] = get_database_credentials()["database"]
-os.environ["PG_HOST"] = get_database_credentials()["hostname"]
-os.environ["PG_PORT"] = str(get_database_credentials()["port"])
+# load_dotenv()
 
 
 def connect_db():
     return pg8000.native.Connection(
-        user=os.getenv("PG_USER"),
-        password=os.getenv("PG_PASSWORD"),
-        database=os.getenv("PG_DATABASE"),
-        host=os.getenv("PG_HOST"),
-        port=os.getenv("PG_PORT"),
+        user=get_database_credentials()["username"],
+        password=get_database_credentials()["password"],
+        database=get_database_credentials()["database"],
+        host=get_database_credentials()["hostname"],
+        port=str(get_database_credentials()["port"])
     )
