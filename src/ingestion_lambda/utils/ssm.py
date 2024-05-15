879905443eb76648b5040fe02aedc574d7d89e6c
mansk <mansk@users.noreply.github.com>
2024-05-15 15:11:56 +0100

    Add util functions to store and retrieve params from ssm

---
+++ src/ingestion_lambda/utils/ssm.py
@@ -0,0 +1,33 @@
+import boto3
+from botocore.exceptions import ClientError
+from .custom_exceptions import *
+
+
+def get_parameter(param: str):
+    ssm_client = boto3.client('ssm')
+
+    try:
+        response = ssm_client.get_parameter(Name=param)
+
+        return response['Parameter']['Value']
+
+    except ClientError as e:
+        error_handler(e)
+
+        raise ClientError(e.response, e.operation_name)
+
+
+def set_parameter(param: str, value: str):
+    ssm_client = boto3.client('ssm')
+
+    try:
+        ssm_client.put_parameter(
+            Name=param,
+            Value=value,
+            Type='String',
+            Overwrite=True)
+
+    except ClientError as e:
+        error_handler(e)
+
+        raise ClientError(e.response, e.operation_name)
