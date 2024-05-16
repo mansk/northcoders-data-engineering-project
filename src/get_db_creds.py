3b4f1bda1419cba0994d1304b13ae8b43e08d7dc
mansk <mansk@users.noreply.github.com>
2024-05-16 09:36:30 +0100

    Fix calling of get_database_credentials in connect.py and test_get_db_creds.py

--- src/get_db_creds.py
+++ src/get_db_creds.py
@@ -1,7 +1,8 @@
 import boto3
 from botocore.exceptions import ClientError
 import json
-'''
+
+"""
 To access db creds using the function:
 
 Pass secret_id e.g. "database_credentials" or "warehouse_credentials"
@@ -13,11 +14,11 @@ Hostname = get_database_credentials()["hostname"]
 Username = get_database_credentials()["username"]
 Password = get_database_credentials()["password"]
 Port = get_database_credentials()["port"]
-'''
+"""
+
+
 def get_database_credentials(secret_id):
-    sm_client = boto3.client(
-        "secretsmanager"
-    )
+    sm_client = boto3.client("secretsmanager")
     try:
         response = sm_client.get_secret_value(SecretId=secret_id)
         db_creds = response["SecretString"]
