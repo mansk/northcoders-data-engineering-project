498e900f23cb0d982878705f6d08df5437190338
mansk <mansk@users.noreply.github.com>
2024-05-17 06:35:23 +0100

    Documentation: Add docstring to connect_db

--- src/ingestion_lambda/utils/connect.py
+++ src/ingestion_lambda/utils/connect.py
@@ -10,7 +10,18 @@ except ModuleNotFoundError:
 # load_dotenv()
 
 
-def connect_db():
+def connect_db() -> pg8000.native.Connection:
+    """Connects to a PostgreSQL database using pg8000.
+
+    This function uses the get_database_credentials function (with the arg
+    "database_credentials") to retrieve the necessary credentials
+
+    Args:
+        None.
+
+    Returns:
+        An instance of the pg8000.native.Connection class.
+    """
     return pg8000.native.Connection(
         user=get_database_credentials("database_credentials")["username"],
         password=get_database_credentials("database_credentials")["password"],
