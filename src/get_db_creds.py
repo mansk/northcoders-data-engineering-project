f7dea584689e547ea4c6fab4e00a5bebf1bc70af
adztborn <144327483+adztborn@users.noreply.github.com>
2024-05-17 15:39:07 +0100

    adding get_db_creds doc string

--- src/get_db_creds.py
+++ src/get_db_creds.py
@@ -1,23 +1,24 @@
 import boto3
 from botocore.exceptions import ClientError
 import json
-
-"""
-To access db creds using the function:
-
-Pass secret_id e.g. "database_credentials" or "warehouse_credentials"
-to the get_database_credentials function - this way we can reuse the
-function when we need to access the warehouse in the future.
-
-Database = get_database_credentials()["database"]
-Hostname = get_database_credentials()["hostname"]
-Username = get_database_credentials()["username"]
-Password = get_database_credentials()["password"]
-Port = get_database_credentials()["port"]
-"""
-
-
 def get_database_credentials(secret_id):
+    """Retrieves database credentials from AWS secrets manager.
+
+    This function retrieves database credentials from the AWS secrets manager.
+    We are using it to retrieve login credentials for the database we are ingesting from and the warehouse we are loading to.
+
+    Args:
+    secret_id: The relevant secret_id is passed into the function to retrieve credentials for the specified secret
+    e.g. database_one, database_two.
+
+    Returns:
+    Credentials in json format, accessible by key e.g.
+    Database = get_database_credentials()["database"]
+    Hostname = get_database_credentials()["hostname"]
+    Username = get_database_credentials()["username"]
+    Password = get_database_credentials()["password"]
+    Port = get_database_credentials()["port"]
+    """
     sm_client = boto3.client("secretsmanager")
     try:
         response = sm_client.get_secret_value(SecretId=secret_id)
