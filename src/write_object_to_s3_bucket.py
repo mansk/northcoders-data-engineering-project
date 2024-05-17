43c6da35e8737350a79b881935689a04cfe965d5
mansk <mansk@users.noreply.github.com>
2024-05-17 06:23:05 +0100

    Documentation: Add docstring to write_object_to_s3_bucket

--- src/write_object_to_s3_bucket.py
+++ src/write_object_to_s3_bucket.py
@@ -11,7 +11,27 @@ except ModuleNotFoundError:
 client = boto3.client("s3")
 
 
-def write_object_to_s3_bucket(bucket_name, file_key, data):
+def write_object_to_s3_bucket(bucket_name: str, file_key: str, data: str) -> str:
+    """Writes a new object to an S3 bucket.
+
+    This function uses the boto3 S3 client put_object method to attempt to
+    write the provided data to provided key in the specified bucket.
+    The data will be UTF-8 encoded.
+
+    Args:
+        bucket_name: A string of the name of the destination S3 bucket.
+        file_key: A string of the target file name (including prefix if
+          applicable) to which to save the data in S3.
+        data: String data to be written to the body of the file.
+
+    Returns:
+        A string reporting success if successful.
+
+    Raises:
+        A custom exception named after the "Code" in the Boto3 error response
+        if found in the exceptions at src.custom_exceptions, otherwise a
+        botocore.exceptions.ClientError
+    """
     try:
         response = client.put_object(
             Body=data.encode("utf_8"),
