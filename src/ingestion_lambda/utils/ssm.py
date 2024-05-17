1007fcc456eede692c742936ad2786e1a31ec392
mansk <mansk@users.noreply.github.com>
2024-05-17 13:40:19 +0100

    Documentation: Added docstrings to get_parameter and set_parameter

--- src/ingestion_lambda/utils/ssm.py
+++ src/ingestion_lambda/utils/ssm.py
@@ -8,6 +8,22 @@ except ModuleNotFoundError:
 
 
 def get_parameter(param: str):
+    """Retrieves a parameter from AWS Systems Manager Parameter Store.
+
+    This function uses the boto3 SSM client get_parameter method to attempt to
+    retrieve the value of the parameter.
+
+    Args:
+        param: A string of the name of the parameter.
+
+    Returns:
+        A string of the value of the parameter.
+
+    Raises:
+        A custom exception named after the "Code" in the Boto3 error response
+        if found in the exceptions at src.custom_exceptions, otherwise a
+        botocore.exceptions.ClientError
+    """
     ssm_client = boto3.client("ssm")
 
     try:
@@ -22,6 +38,23 @@ def get_parameter(param: str):
 
 
 def set_parameter(param: str, value: str):
+    """Stores a parameter in AWS Systems Manager Parameter Store.
+
+    This function uses the boto3 SSM client put_parameter method to attempt to
+    store the value of the parameter.
+
+    Args:
+        param: A string of the name of the parameter.
+        value: A string of the value to be set for the parameter.
+
+    Returns:
+        None.
+
+    Raises:
+        A custom exception named after the "Code" in the Boto3 error response
+        if found in the exceptions at src.custom_exceptions, otherwise a
+        botocore.exceptions.ClientError
+    """
     ssm_client = boto3.client("ssm")
 
     try:
