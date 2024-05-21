83975001eb96b4816dd627165f915b47f4859cae
14doan <14doan@gmail.com>
2024-05-21 07:18:38 +0100

    add log permission to process-lambda

--- terraform/processed_lambda.tf
+++ terraform/processed_lambda.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_function" "processed_lambda" {
-  function_name    = "${var.team_prefix}ingestion-lambda"
+  function_name    = "${var.team_prefix}processed-lambda"
   filename         = n/a
   source_code_hash = n/a
   role             = aws_iam_role.processed_lambda_role.arn
