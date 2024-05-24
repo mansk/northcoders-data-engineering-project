47dc94165375db3f11a9ce7b506f4a804f8eb86b
mansk <mansk@users.noreply.github.com>
2024-05-24 16:08:49 +0100

    Incorporate schema in loading_to_sql function

--- src/loading_lambda/utils/loading_to_sql.py
+++ src/loading_lambda/utils/loading_to_sql.py
@@ -1,15 +1,19 @@
 import pandas as pd
-from src.connect import connect_db
-
-
+from src.get_db_creds import get_database_credentials
 
 
 def loading_to_sql(table_name, conn, df):
-    
-    #schema = get_database_credentials("data_warehouse_credentials")["schema"]
+
+    schema = get_database_credentials("data_warehouse_credentials")["schema"]
 
     try:
-        df.to_sql(name=table_name, con=conn, if_exists='append')
-    
+        df.to_sql(
+            name=table_name,
+            con=conn,
+            index=False,
+            if_exists="append",
+            schema=schema,
+        )
+
     except Exception as e:
         raise e
